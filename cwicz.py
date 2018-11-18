2f4764f2727220d06b6deb5b0f0fdaf698ceddaf
radam <biuro@kolektorowo.pl>
2018-11-18 10:21:10 +0100

    My Django Girls app, first commit

---
+++ cwicz.py
@@ -0,0 +1,10 @@
+def plik(imie):
+    print('czesc'+imie+'!!!')
+
+
+osoby=['adam','Zuza','Jola','franek']
+for imie in osoby:
+    plik(imie)
+    print ('next')
+for i in range(1, 6):
+    print (i)
